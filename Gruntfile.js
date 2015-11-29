b2c2927bbc77159c160cc92c15cfd59f4b439374
Di Grégorio Thomas <thomas@devingfx.com>
2015-11-29 13:35:22 +0100

    On branch master 	Repository refactory  Changes to be committed: 	new file:   .gitignore 	new file:   Gruntfile.js 	moved:      * -> src/

---
+++ Gruntfile.js
@@ -0,0 +1,44 @@
+module.exports = function(grunt) {
+
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		concat: {
+			options: {
+				separator: ';\n',
+			},
+			AppKit: {
+				src: [
+					'src/js/AppKit/Class.js',
+					'src/js/AppKit/EventDispatcher.js',
+					'src/js/AppKit/NodeClass.js',
+					'src/js/AppKit/ScrollPane.js',
+					'src/js/AppKit/WebApp.js',
+					'src/js/AppKit/PageView.js',
+					'src/js/AppKit/Page.js',
+					'src/js/AppKit/Menu.js',
+					// 'src/libs/sass.sync.js',
+					// 'src/js/AppKit/StyleSass.js',
+					'src/js/AppKit.js',
+				],
+				dest: 'dist/AppKit.src.js',
+			}
+		},
+		
+		uglify: {
+			AppKit: {
+				files: {
+					'dist/AppKit.js': ['dist/AppKit.src.js']
+				}
+			}
+		}
+	});
+
+  // Load the plugins that provides the "uglify" task and "concat" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  
+  // Default task(s).
+  grunt.registerTask('default', ['concat:AppKit']);
+
+};
